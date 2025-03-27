a3716566bb4fd5ee8b4813ca21c4867284715c1a
Tamzidul Matin <tmatin100@gmail.com>
2025-03-27 19:00:39 -0400

    Add analysis script

--- analysis.py
+++ analysis.py
@@ -1,9 +1,7 @@
-def farewell(name):
-    return f"Goodbye, {name}!"
+def greet(name):
+    return f"Hello, {name}!"
 
 if __name__ == "__main__":
     user_name = input("Enter your name: ")
     print(greet(user_name))
-
-print(farewell(user_name))
 
