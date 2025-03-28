8cbfcc051d7f07bb2be000bbffe5ea9616e9727f
Tamzidul Matin <tmatin100@gmail.com>
2025-03-27 20:00:15 -0400

    Add sum_values function to analysis script

--- analysis.py
+++ analysis.py
@@ -1,12 +1,7 @@
-def greet(name):
-    return f"Goodbye, {name}!"
+import pandas as pd
 
-if __name__ == "__main__":
-    user_name = input("Enter your name: ")
-    print(farewell (user_name))
+def sum_values():
+    df = pd.read_csv('data/sample_data.csv')
+    return df['value'].sum()
 
-
-
-
-
-
+print(f"Sum of values: {sum_values()}")
